200a2ecc1918e3119ad745d0ce4bb745bb3d5b8e
Sential <sentialx@gmail.com>
2016-08-31 01:54:02 +0200

    small history layout fixes

--- js/history.js
+++ js/history.js
@@ -24,8 +24,8 @@ var json;
                     var item = $('<div class="item" style="padding: 8px;">').appendTo('.card');
                     var border = $('<div style="display: inline; height:14px; padding-left:18px; border-radius: 2px; border: 2px solid #9E9E9E;"></div>').appendTo(item);
                     var hour = $('<p style="display: inline;margin-left: 16px; color: #9E9E9E;">21:37</p>').appendTo(item);
-                    var title = $('<p style="display: inline; margin-left: 48px;">' + jsonItem.title + '</p>').appendTo(item);
-                    var title = $('<a href="' + jsonItem.link + '" style="margin-left: 8px;">' + jsonItem.link + '</a>').appendTo(item);
+                    var title = $('<p style="display: inline; margin-left: 48px;overflow: hidden;white-space: nowrap;">' + jsonItem.title + '</p>').appendTo(item);
+                    var link = $('<a href="' + jsonItem.link + '" style="margin-left: 8px; display: inline; margin-right: 32px; overflow: hidden;white-space: nowrap;">' + jsonItem.link + '</a>').appendTo(item);
 
                   }
 
