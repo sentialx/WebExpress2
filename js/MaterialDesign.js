51294fd77f677b28d37bb2a9f25bc5a49adb6d13
Sential <sentialx@gmail.com>
2016-09-04 21:48:40 +0200

    material design switch fixes

--- js/MaterialDesign.js
+++ js/MaterialDesign.js
@@ -1,161 +1,107 @@
 $(document).ready(function() {
-  //declare objects
-  var eSwitch = $(".mdc-switch");
 
-  /* checkbox */
+    //Switch
+    //variables
+    class Switch {
+        constructor(where) {
+            where.switched = false;
+            var instance = $("<div>").load("controls/switch.html", function() {
+                var mdSwitch = instance.find(".md-switch");
+                var ellipseRipple = instance.find('.ellipse-ripple');
+                ellipseRipple.hide("scale", {
+                    procent: 0
+                }, 1);
 
-  var CheckBox = class CheckBox {
+                //events
+                mdSwitch.click(function() {
+                    var switchContainer = $(this).find('.switch-container');
+                    var lever = $(this).find('.lever');
+                    ellipseRipple = $(this).find('.ellipse-ripple');
+                    var ellipse = $(this).find('.ellipse');
+                    if (!where.switched) {
+                        ellipseRipple.stop();
+                        ellipseRipple.css({
+                            height: 48,
+                            width: 48,
+                            left: -17.5 + 28,
+                            top: -15,
+                            position: 'absolute'
+                        });
+                        ellipse.animate({
+                            backgroundColor: mdSwitch.attr('ellipse-color')
+                        }, {
+                            queue: false,
+                            duration: 200
+                        });
+                        ellipse.animate({
+                            left: 24
+                        }, {
+                            queue: false,
+                            duration: 200
+                        });
+                        lever.animate({
+                            backgroundColor: mdSwitch.attr('bg-color-on')
+                        }, {
+                            queue: false,
+                            duration: 200
+                        });
+                        ellipseRipple.css('opacity', 0.4).hide().show("scale", {
+                            procent: 100
+                        }, 200, function() {
+                            ellipseRipple.animate({
+                                opacity: 0
+                            });
+                        });
 
-    static checked(element, bool) {
-      var t = element.data('can-check');
+                        where.switched = true;
+                    } else {
+                        ellipseRipple.stop();
+                        ellipseRipple.css({
+                            height: 48,
+                            width: 48,
+                            left: -17.5,
+                            top: -15,
+                            position: 'absolute'
+                        });
+                        ellipse.animate({
+                            backgroundColor: mdSwitch.attr('ellipse-basecolor')
+                        }, {
+                            queue: false,
+                            duration: 200
+                        });
+                        ellipse.animate({
+                            left: -4
+                        }, {
+                            queue: false,
+                            duration: 200
+                        });
+                        lever.animate({
+                            backgroundColor: mdSwitch.attr('bg-color-off')
+                        }, {
+                            queue: false,
+                            duration: 200
+                        });
+                        ellipseRipple.css('opacity', 0.4).hide().show("scale", {
+                            procent: 100
+                        }, 200, function() {
+                            ellipseRipple.animate({
+                                opacity: 0
+                            });
+                        });
 
-      var c = false;
-      if (t == true || t == null) {
-        c = true;
-      } else {
-        c = false;
-      }
-      if (1) { //c == true
-        element.addClass('ccheck');
-        var ellipse = element.find(".ellipse");
-        var ellipseRipple = element.find(".ellipse-ripple");
-        var lever = element.find(".lever");
-
-        var DataToggleOffColor = element.data('toggle-off-color');
-        var DataEllipseToggleOffColor = element.data('ellipse-toggle-off-color');
-        var DataToggleOnColor = element.data('toggle-on-color');
-        var DataEllipseToggleOnColor = element.data('ellipse-toggle-on-color');
-
-        if (bool) {
-          ellipseRipple.addClass("isanimating");
-          ellipseRipple.animate({opacity: '0.2'}, 300, function() {
-            ellipseRipple.animate({ left: '12px' }, 200, 'easeInOutQuart',function() {
-
-            });
-            lever.animate({backgroundColor: DataToggleOnColor}, {duration: 300, queue: false});
-            ellipse.animate({backgroundColor: DataEllipseToggleOnColor},{duration: 300, queue: false});
-            ellipseRipple.animate({backgroundColor: DataEllipseToggleOnColor}, {duration: 300, queue: false});
-
-            ellipse.animate({ left: '24px' }, 200, 'easeInOutQuart',function() {
-              setTimeout(function() {
-                ellipseRipple.animate({opacity: '0'}, 300, function() {
-                   ellipseRipple.removeClass("isanimating");
-                   setTimeout(function() {
-                     element.removeClass('ccheck');
-                   }, 1);
+                        where.switched = false;
+                    }
                 });
-              }, 250);
-            });
-          });
-        } else {
-          ellipseRipple.addClass("isanimating");
-          ellipseRipple.animate({opacity: '0.2'}, 300, function() {
-            ellipseRipple.animate({ left: '-17.5px' }, 200, 'easeInOutQuart',function() {
+            }).appendTo($(where));
 
-            });
-            lever.animate({backgroundColor: DataToggleOffColor}, {duration: 300, queue: false});
-            ellipse.animate({backgroundColor: DataEllipseToggleOffColor},{duration: 300, queue: false});
-            ellipseRipple.animate({backgroundColor: DataEllipseToggleOffColor}, {duration: 300, queue: false});
-            ellipse.animate({ left: '-4px' }, 200, 'easeInOutQuart',function() {
-
-              setTimeout(function() {
-                ellipseRipple.animate({opacity: '0'}, 300, function() {
-                  ellipseRipple.removeClass("isanimating");
-                  setTimeout(function() {
-                    element.removeClass('ccheck');
-                  }, 1);
-
-
-                });
-              }, 250);
-            });
-          });
         }
-      }
     }
-  }
+    $('.switch').each(function(index) {
+        var s = new Switch(this);
+    });
+    $('.switch').click(function() {
+      console.log(this.switched);
+    })
 
-  //events
-  var x = true;
 
-  /*eSwitch.mousedown(function(event) {
-    CheckBox.showRipple($(this), x);
-    if (x == true) {
-      x = false;
-    } else {
-      x = true;
-    }
-  });
-
-  eSwitch.mouseup(function(event) {
-    CheckBox.showRipple($(this), false);
-  });*/
-  var x = true;
-  eSwitch.click(function() {
-    var ellipse = $(this).find(".ellipse");
-    var ellipseRipple = $(this).find(".ellipse-ripple");
-
-    var isanimating = ellipseRipple.hasClass('isanimating');
-    console.log(isanimating);
-
-    if (isanimating == false) {
-      CheckBox.checked($(this), x);
-    }
-    if (x) { x = false; } else { x = true; }
-  });
-  eSwitch.mousedown(function() {
-    var ellipse = $(this).find(".ellipse");
-    var ellipseRipple = $(this).find(".ellipse-ripple");
-
-  /*  ellipseRipple.animate({opacity: '0.2'}, 300, function() {
-
-  });*/
-  });
-  eSwitch.mouseleave(function() {
-    var ellipse = $(this).find(".ellipse");
-    var ellipseRipple = $(this).find(".ellipse-ripple");
-    var hc = ellipseRipple.hasClass("isanimating");
-    if (hc == false || hc == null) {
-      ellipseRipple.animate({opacity: '0'}, 300, function() {
-
-      });
-    }
-  });
-
-  eSwitch.each(function() {
-    var ellipse = $(this).find(".ellipse");
-    var ellipseRipple = $(this).find(".ellipse-ripple");
-    var lever = $(this).find(".lever");
-
-    var ToggleOffColor = lever.css('background-color');
-    var EllipseToggleOffColor = ellipseRipple.css('background-color');
-    var ToggleOnColor = "#1abc9c";
-    var EllipseToggleOnColor = "#16a085";
-
-    var DataToggleOffColor = $(this).data('toggle-off-color');
-    var DataEllipseToggleOffColor = $(this).data('ellipse-toggle-off-color');
-    var DataToggleOnColor = $(this).data('toggle-on-color');
-    var DataEllipseToggleOnColor = $(this).data('ellipse-toggle-on-color');
-
-    if (DataToggleOffColor != null || typeof DataToggleOffColor !== "undefined") {
-      ToggleOffColor = DataToggleOffColor;
-    }
-
-    if (DataToggleOnColor != null || typeof DataToggleOnColor !== "undefined") {
-      ToggleOnColor = DataToggleOnColor;
-    }
-
-    if (DataEllipseToggleOffColor != null || typeof DataEllipseToggleOffColor !== "undefined") {
-      EllipseToggleOffColor = DataEllipseToggleOffColor;
-    }
-
-    if (DataEllipseToggleOnColor != null || typeof DataEllipseToggleOnColor !== "undefined") {
-      EllipseToggleOnColor = DataEllipseToggleOnColor;
-    }
-
-    lever.css('background-color', ToggleOffColor);
-    ellipse.css('background-color', EllipseToggleOffColor);
-    ellipseRipple.css('background-color', EllipseToggleOffColor);
-  });
-
-});
+})
